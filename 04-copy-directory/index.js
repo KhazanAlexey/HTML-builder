c9214c659d93cb0c9fc6ae41366feb4ca7b3e7c1
Alexey <64707536+KhazanAlexey@users.noreply.github.com>
2022-05-22 22:55:59 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const path = require('path');
+const fs = require('fs');
 
+
+function copyDir() {
+  const allFiles = fs.promises.readdir(path.join(__dirname), {withFileTypes: true});
+  allFiles.then((files) => {
+    for (let file of files) {
+      if (!file.isFile() && file.name !== 'files-copy') {
+        const makeDir = fs.promises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
+        makeDir.then(() => {
+          readFile();
+        });
+      }
+    }
+  });
+
+  function readFile() {
+    const filesDir = fs.promises.readdir(path.join(__dirname, 'files'), {withFileTypes: true});
+    filesDir.then((files) => {
+      for (let file of files) {
+        if (file.isFile()) {
+          console.log(file);
+          copyFile(file.name);
+        }
+
+      }
+    });
+  }
+
+  function copyFile(file) {
+    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+
+      if (err) console.log(err);
+
+    });
+  }
+}
+
+copyDir();
